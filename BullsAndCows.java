2abae4ade90bf047eea9b8017567321d96e1b600
sdrobyshevsky <sdrobyshevsky@mail.ru>
2023-10-05 20:37:38 +0300

    Added info about task

--- BullsAndCows.java
+++ BullsAndCows.java
@@ -58,4 +58,22 @@ public class BullsAndCows
 		}
 		return false;
 	}
+
+
+    {
+        Menu.printUserMenu();
+        NumberGame numberGame = new NumberGame();
+        try (Scanner scanner = new Scanner(System.in)) {
+            numberGame.start(scanner.nextInt(), scanner.nextInt());
+            scanner.nextLine();
+            while (!numberGame.getGameStatus().equals(GameStatus.FINISH)){
+                String value = scanner.nextLine();
+                Answer answer = numberGame.inputValue(value);
+                if (answer!=null){
+                    System.out.println("answer = " + answer);
+                }
+                System.out.println(numberGame.getMoveHistory());
+            }
+        }
+    }
 }
